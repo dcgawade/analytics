2841d26033ae9e00b197e789075166feefa9cd71
dcgawade <dineshgawade244@gmail.com>
2018-04-28 16:43:43 +0530

    project on decision tree

---
+++ DT-CART-class.R
@@ -0,0 +1,13 @@
+#Decision Tree
+## Finding how people take decision to buy Products
+#Creating Decision Tree
+
+library(rpart) # for DEcision tree CART
+library(rpart.plot) # For Drawing plots for DT 
+library(RColorBrewer) #enhancing color  of plots
+
+
+#Student : Gender - (Male & Female) buy a product
+
+#variable Gender 
+set.seed(100)
